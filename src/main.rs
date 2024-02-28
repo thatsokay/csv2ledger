6d3c9408b152bc7edec6f2f9e4a5dc4148f198c3
Andre Wong <andre@andre-wong.com>
2024-02-28 18:31:55 +1000

    Factored out conversion

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use serde::Deserialize;
 use std::env;
 use std::error::Error;
 use std::ffi::OsString;
+use std::fmt::Write as _;
+use std::io::Read;
 use std::process;
 
 #[derive(Debug, Deserialize)]
@@ -26,17 +28,9 @@ fn main() {
 
 fn run() -> Result<(), Box<dyn Error>> {
     let file_path = get_first_arg()?;
-    let mut rdr = csv::Reader::from_path(file_path)?;
-    for result in rdr.deserialize() {
-        let record: Record = result?;
-        if let (Some(date), Some(description)) = (record.date, record.description) {
-            println!("\n{} {}", date, description);
-        }
-        if let Some(comment) = record.comment {
-            println!("    ; {}", comment);
-        }
-        println!("    {}    ${:.2}", record.account, record.amount);
-    }
+    let reader = csv::Reader::from_path(file_path)?;
+    let ledger = convert(reader)?;
+    print!("{}", ledger);
     Ok(())
 }
 
@@ -47,4 +41,23 @@ fn get_first_arg() -> Result<OsString, Box<dyn Error>> {
         None => Err(From::from("expected 1 argument, but got none")),
         Some(file_path) => Ok(file_path),
     }
+}
+
+fn convert<R: Read>(mut reader: csv::Reader<R>) -> Result<String, Box<dyn Error>> {
+    let mut ledger = String::new();
+    for result in reader.deserialize() {
+        let record: Record = result?;
+        if let (Some(date), Some(description)) = (record.date, record.description) {
+            write!(&mut ledger, "\n{} {}\n", date, description)?;
+        }
+        if let Some(comment) = record.comment {
+            write!(&mut ledger, "    ; {}\n", comment)?;
+        }
+        write!(
+            &mut ledger,
+            "    {}    ${:.2}\n",
+            record.account, record.amount
+        )?;
+    }
+    Ok(ledger)
 }
